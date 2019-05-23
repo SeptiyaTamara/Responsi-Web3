53dc3bd36972b84dda97c61106d63e98b2936d3f
septiyaTamara <septiyakristin98@mail.ugm.ac.id>
2019-05-23 09:57:01 +0700

    Updates

---
+++ precache-manifest.2f233154f9e0a32879e62b63c2b028d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bafe6b9b82fe2a1349d267324e2f6798",
+    "url": "/Responsi-Web3/index.html"
+  },
+  {
+    "revision": "0dd37ccf60bbd7a33914",
+    "url": "/Responsi-Web3/static/css/main.ba49ec77.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/Responsi-Web3/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "0dd37ccf60bbd7a33914",
+    "url": "/Responsi-Web3/static/js/main.b4cc6f8b.chunk.js"
+  },
+  {
+    "revision": "9eb1e7eb85fa76014adf",
+    "url": "/Responsi-Web3/static/js/runtime~main.c9636149.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/Responsi-Web3/static/media/logo.ee7cd8ed.svg"
+  }
+]);
